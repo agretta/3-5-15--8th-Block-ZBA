fbd645f98842b62a6e0f96c35e6c5c81aec2d82b
Zahin Nambiar <s511383@student.roundrockisd.org>
2015-04-02 15:49:21 -0500

    Back End v1

--- src/BackEnd.java
+++ src/BackEnd.java
@@ -1,4 +1,54 @@
+import java.util.ArrayList;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
 
 public class BackEnd {
+	
+	
+	public BackEnd ()
+	{
+		
+		
+		
+		
+	}
+	
+	public List <String>  getMessages(String username)
+	{
+		
+		ArrayList array1 = new ArrayList <String> ();
+		
+		
+		return array1;	
+		
+		
+		
+	}
+	public void addMessage(String username, String msg)
+	{
+		
+		
+		
+		
+	}
+	public List <String> getUsers()
+	{
+		String array = new ArrayList <String> ();
+		
+		
+		return array;
+	}
+public static void main(String args[]){
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
