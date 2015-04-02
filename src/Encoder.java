6e8743f84bde21919d1510fda0db403331dd3c39
agretta <grexxaa@gmail.com>
2015-04-02 15:28:55 -0500

    Added Encoder
    and meathod stubs

---
+++ src/Encoder.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+public class Encoder {
+	
+	public Encoder(){
+		
+		
+		
+		
+	}
+	
+	
+	public void write(String plainText){
+		
+		
+		
+		
+	}
+	
+	public List<String> read(){
+		
+		
+		
+		return new ArrayList<String>();
+	}
+	
+	
+	
+}
