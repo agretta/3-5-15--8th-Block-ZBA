fd8db254b1a5d954567af6f69ad954ae1488047a
agretta <grexxaa@gmail.com>
2015-04-02 22:20:30 -0500

    I Was Bored
    I Kinda did the whole project, but didnt save it to this repository so i
    wouldnt take your guys' work

--- src/Encoder.java
+++ src/Encoder.java
@@ -3,28 +3,21 @@ import java.io.*;
 public class Encoder {
 	
 	private File myFile;
-	private static String source=" abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\t";
-	private static String target=" zyxwvutsrqponmlkjihgfedcbaQ9A8ZWS7XEDC6RFVT5GBY4HNU3J2MI1KO0LP\t";
+	private static String source=" abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\t=";
+	private static String target=" zyxwvutsrqponmlkjihgfedcbaQ9A8ZWS7XEDC6RFVT5GBY4HNU3J2MI1KO0LP\t*";
 	
 	
 	public Encoder(File f){
 		myFile = f;
-		
-		
-		
 	}
 	
 	
 	public void write(String plainText){
-		try
-		{
-			PrintWriter printer = new PrintWriter(myFile);
-			printer.println(this.obfuscate(plainText));
-		}
-		catch (FileNotFoundException e) {e.printStackTrace();}
-		
-		
-		
+		try {
+			PrintWriter print = new PrintWriter(new BufferedWriter(new FileWriter(myFile,true)));
+			print.println(this.obfuscate(plainText));
+			print.close();
+		} catch (IOException e) {e.printStackTrace();}
 	}
 	
 	public List<String> read(){
@@ -35,6 +28,7 @@ public class Encoder {
 			while(scan.hasNextLine()){
 				list.add( unobfuscate(scan.nextLine()));
 			}
+			scan.close();
 		}
 		catch(FileNotFoundException e) {e.printStackTrace();}
 		
